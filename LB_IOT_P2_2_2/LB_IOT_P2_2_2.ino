e9899c89ba90278d1077af9704b8357644300f25
Miguel-Lorenzo-869223 <869223@unizar.es>
2026-09-21 22:13:38 +0200

    Práctica 2.2.2
    Creación de un programa que cree dos tareas, una que parpadee un LED cada 200 ms y otra que envíe un "Hola mundo" por la UART cada segundo.

--- LB_IOT_P2_2_2/LB_IOT_P2_2_2.ino
+++ LB_IOT_P2_2_2/LB_IOT_P2_2_2.ino
@@ -7,14 +7,14 @@
 // ------------------------------------------------------------
 
 /*
- * Microcontroller: ESP32
+ * Microcontroller: ESP32 WROOM 
  * OS: FreeRTOS
  * Description: Multitasking program executing two independent concurrent tasks.
  *   - Task 1 (Blink_LED): Toggles an onboard LED (GPIO 2) every 200 ms.
- *   - Task 2 (Envio_UART): Sends "Hola mundo" over UART (115200 baud) every 1000 ms.
+ *   - Task 2 (Printer_UART): Sends "HELLO WORLD" over UART (115200 baud) every 1000 ms.
  *
  * xTaskCreate parameters breakdown:
- *   1. pvTaskCode    (TareaBlinkLED) : Pointer to the function that implements the task.
+ *   1. pvTaskCode    (task_blink_led) : Pointer to the function that implements the task.
  *   2. pcName        ("Blink_LED")   : Descriptive string name for debugging purposes.
  *   3. usStackDepth  (2048)          : Stack size reserved for the task in bytes.
  *   4. pvParameters  (NULL)          : Pointer to argument variables passed into the task.
@@ -25,9 +25,12 @@
 #include <Arduino.h>
 
 #define LED_PIN 23
+#define TIME_BLINK 200
+#define TIME_PRINTER 1000
 
-void TareaBlinkLED(void *parameter);
-void TareaUART(void *parameter);
+//FUNCTIONS DECLARATION
+void task_blink_led(void *parameter);
+void task_printer_uart(void *parameter);
 
 void setup() {
   Serial.begin(115200);
@@ -35,29 +38,29 @@ void setup() {
 
   pinMode(LED_PIN, OUTPUT);
 
-  xTaskCreate(TareaBlinkLED, "Blink_LED", 2048, NULL, 1, NULL);
-  xTaskCreate(TareaUART, "Envio_UART", 2048, NULL, 1, NULL);
+  xTaskCreate(task_blink_led, "Blink_LED", 2048, NULL, 1, NULL);
+  xTaskCreate(task_printer_uart, "Printer_UART", 2048, NULL, 1, NULL);
 }
 
 void loop() {
-  vTaskDelay(pdMS_TO_TICKS(1000));
+  vTaskDelay(pdMS_TO_TICKS(TIME_PRINTER));
 }
 
-void TareaBlinkLED(void *parameter) {
+void task_blink_led(void *parameter) {
   for (;;) {
     digitalWrite(LED_PIN, HIGH);
-    vTaskDelay(pdMS_TO_TICKS(200));
+    vTaskDelay(pdMS_TO_TICKS(TIME_BLINK));
 
     digitalWrite(LED_PIN, LOW);
-    vTaskDelay(pdMS_TO_TICKS(200));
+    vTaskDelay(pdMS_TO_TICKS(TIME_BLINK));
   }
   vTaskDelete(NULL);
 }
 
-void TareaUART(void *parameter) {
+void task_printer_uart(void *parameter) {
   for (;;) {
-    Serial.println("Hola mundo");
-    vTaskDelay(pdMS_TO_TICKS(1000));
+    Serial.println("HELLO WORLD");
+    vTaskDelay(pdMS_TO_TICKS(TIME_PRINTER));
   }
   vTaskDelete(NULL);
 }
